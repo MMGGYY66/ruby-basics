d0dd9f40f7e1d839e3f92314ddaa6f7ec2c7c0a1
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-04-01 16:23:00 -0500

    Initial commit

---
+++ practice.rb
@@ -0,0 +1,10 @@
+puts "Hello Ruby"
+puts "Hello #{RUBY_VERSION}"
+puts "Hello #{RUBY_RELEASE_DATE}"
+puts "Hello #{RUBY_PATCHLEVEL}"
+
+puts "Hello #{RUBY_DESCRIPTION}"
+
+puts "Hello #{RUBY_COPYRIGHT}"
+
+
